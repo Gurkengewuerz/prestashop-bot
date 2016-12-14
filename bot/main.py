d8f52a9660603f560bc153d995c944a2a5e659ac
LtPhoenic <lt.phoenic@googlemail.com>
2016-12-14 10:23:19 +0100

    started mysql connection rip battery :( ujelly

--- bot/main.py
+++ bot/main.py
@@ -6,13 +6,23 @@ db = DB()
 
 if __name__ == "__main__":
     db.connect()
-    shop = Shop(shop_url, api_key, [2, 4], None)
-    if not shop.canConnect(): sys.exit("Can not connect to shop!")
-
-    for order in shop.getorders():
-        print(order.sendMSG("This is a test msg"))
-        for item in order.list_items:
-            if True and True:  # TODO: IF IN DB AND COUNT IS EQUAL OR HIGHER (db.query("SELECT * FROM ita_?")
-                pass
-
+    #shop = Shop(shop_url, api_key, [2, 4], None)
+    shopQuery = db.query("SELECT * FROM ita_shop;")
+    for shop in shopQuery.fetchall():
+        statQuery = db.query("SELECT * FROM ita_shop_stat WHERE shop_id=%s"%shop[0])
+        statlist = list()
+        for stat in statQuery.fetchall():
+              statlist.append(stat[2])
+        print(shop)
+        shopObj = Shop(shop[1], shop[2], statlist, shop[4])
+        if not shopObj.canConnect(): sys.exit("Can not connect to shop!")
+        for order in shopObj.getorders():
+            for item in order.list_items:
+                licenceQuery = db.query("SELECT * FROM ita_licencekey WHERE product_reference = '%s' AND rented_timestamp IS NULL"%item.reference)
+                licenceFetch = licenceQuery.fetchall()
+                if len(licenceFetch) >= item.quantity:
+                    pass # TODO: GET KEYS AND SEND THEM TO ORDER -> UPDATE
+                else:
+                    print("No Product Keys available")
+        break
     db.close()
--- bot/prestaobjects.py
+++ bot/prestaobjects.py
@@ -98,7 +98,7 @@ class Shop():
     def getorders(self):
         for state in self.list_paystat:
             xmlData = self.getxml("orders?display=full&filter[current_state]=%s" % state).find("orders")
-            if len(xmlData) <= 0: return
+            if len(xmlData) <= 0: continue
             for order in xmlData.findall("order"):
                 id = order.find("id")
                 id_customer = order.find("id_customer")
@@ -110,6 +110,5 @@ class Shop():
                     quantity = item.find("product_quantity")
                     pro = Product(reference.text, name.text, int(quantity.text))
                     list_items.append(pro)
-                self.list_orders.append(
-                    Order(self, int(id.text), int(id_customer.text), list_items, order_reference.text))
+                self.list_orders.append(Order(self, int(id.text), int(id_customer.text), list_items, order_reference.text))
         return self.list_orders
