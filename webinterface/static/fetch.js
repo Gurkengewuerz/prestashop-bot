24397acca08a44b728758762f84591fe53e2a302
Niklas <niklas@schuetrumpf.net>
2016-12-28 18:10:46 +0100

    some enhancement TODO: Modal to add things

--- webinterface/static/fetch.js
+++ webinterface/static/fetch.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
     var type = "shop";
 
+    function formatDate(date) {
+        var engMonth = {
+            "Jan": "01",
+            "Feb": "02",
+            "Mar": "03",
+            "Apr": "04",
+            "May": "05",
+            "Jun": "06",
+            "Jul": "07",
+            "Aug": "08",
+            "Sep": "09",
+            "Oct": "10",
+            "Nov": "11",
+            "Dec": "12"
+        };
+        dateArray = date.toString().split(" ");
+        return dateArray[2] + "." + engMonth[dateArray[1]] + "." + dateArray[3] + " " + dateArray[4];
+    }
+
     function loadShop() {
         $.ajax({
             url: '/api/get/shop',
@@ -13,16 +32,16 @@ $(document).ready(function () {
                     console.log(item);
                     var payStats = "";
                     item.paystat.forEach(function (val) {
-                       payStats += "<li>" + val + "</li>";
+                        payStats += "<li>" + val + "</li>";
                     });
                     $("#shopData").append(
                         "<tr>" +
-                            "<td>"+item.id+"</td>" +
-                            "<td>"+item.shop_name+"</td>" +
-                            "<td>"+item.url+"</td>" +
-                            "<td>"+item.api_key+"</td>" +
-                            "<td><ul>"+ payStats + "</ul></td>" +
-                            "<td>"+item.delivstat+"</td>" +
+                        "<td>" + item.id + "</td>" +
+                        "<td>" + item.shop_name + "</td>" +
+                        "<td>" + item.url + "</td>" +
+                        "<td>" + item.api_key + "</td>" +
+                        "<td><ul>" + payStats + "</ul></td>" +
+                        "<td>" + item.delivstat + "</td>" +
                         "</tr>"
                     );
                     console.log(item.id);
@@ -42,16 +61,18 @@ $(document).ready(function () {
             success: function (data) {
                 console.log(data);
                 $("#licenceData tbody > tr").remove();
-                $("#licenceData").append( // TODO: WORK
-                        "<tr>" +
-                            "<td>"+item.id+"</td>" +
-                            "<td>"+item.shop_name+"</td>" +
-                            "<td>"+item.url+"</td>" +
-                            "<td>"+item.api_key+"</td>" +
-                            "<td><ul>"+ payStats + "</ul></td>" +
-                            "<td>"+item.delivstat+"</td>" +
+                $.each(data, function (i, item) {
+                    var disabled = item.rendet_timestamp == null ? "success" : "danger";
+                    $("#licenceData").append(
+                        "<tr class ='" + disabled + "'>" +
+                        "<td>" + item.id + "</td>" +
+                        "<td>" + item.licence + "</td>" +
+                        "<td>" + item.product_reference + "</td>" +
+                        "<td>" + formatDate(new Date(item.created_timestamp * 1000)) + "</td>" +
+                        "<td>" + (item.rendet_timestamp == null ? "nicht vergeben" : formatDate(new Date(item.rendet_timestamp * 1000))) + "</td>" +
                         "</tr>"
                     );
+                });
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(textStatus, errorThrown);
@@ -85,4 +106,9 @@ $(document).ready(function () {
         type = "licence";
         refresh();
     });
+
+    $("#addLicence").click(function () {
+        $('#addLicenceModal').modal("toggle");
+    });
+
 });
